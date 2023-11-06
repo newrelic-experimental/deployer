456e4ce63ed35569b127878ee55d9bd08416e8d9
Mike Bruzina <mbruzina@newrelic.com>
2023-11-06 10:09:22 -0600

    chore(aws): update deployer output for ease of creating Cloudwatch Rules

--- src/teardown/summary.rb
+++ src/teardown/summary.rb
@@ -16,7 +16,7 @@ module Teardown
 
     def get_deploy_config_url(deploy_config_url)
       output = ""
-      output += "deploy config url: #{deploy_config_url}\n" unless deploy_config_url.empty?
+      output += " { \"deploy_config_url\": \"#{deploy_config_url}\", \"deployment_action\": \"teardown\" } " unless deploy_config_url.empty?
       return output
     end
 
